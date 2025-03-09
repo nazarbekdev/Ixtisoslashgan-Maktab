5369a7f1c81d8243a3861c96840cb6c0fd349aa6
nazarbekdev <nqobulov571@gmail.com>
2025-03-10 00:00:21 +0500

    Ixtisoslashgan Maktab Api

---
+++ accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'subject', 'region', 'school', 'class_id', 'gender']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            phone_number=validated_data.get('phone_number', ''),
+            role=validated_data.get('role', 'student'),
+            subject=validated_data.get('subject', ''),
+            region=validated_data.get('region', ''),
+            school=validated_data.get('school', ''),
+            class_id=validated_data.get('class_id', ''),
+            gender=validated_data.get('gender', '')
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
---
+++ auth/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'role', 'phone_number', 'email', 'first_name', 'last_name', 'subject', 'region', 'school', 'class_id', 'gender']
+    list_filter = ['role', 'gender']
+    search_fields = ['username', 'phone_number', 'email']
+    
