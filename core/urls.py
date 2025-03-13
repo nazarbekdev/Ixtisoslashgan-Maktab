3afb56c51a5cb7f5ffe3a3c0493a0c6a6ea94305
nazarbekdev <nqobulov571@gmail.com>
2025-03-13 20:13:47 +0500

    get url refresh token

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,10 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,10 +25,13 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # /api/token/
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # /api/token/refresh/
     path('auth/', include('accounts.urls')), # accounts uchun URL’lar
     path('courses/', include('courses.urls')), # courses uchun URL’lar
     path('news/', include('news.urls')), # news uchun URL’lar
     path('contacts/', include('contacts.urls')), # contacts uchun URL’lar
+    path('students/', include('students.urls')), # students uchun URL’lar
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
