156bccfdc6c86be25d9336ed8a4e07f888e9ee4c
nazarbekdev <nqobulov571@gmail.com>
2025-03-13 20:11:44 +0500

    changed models

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username', 'role', 'phone_number', 'email', 'first_name', 'last_name', 'subject', 'region', 'school', 'class_id', 'gender']
+    list_display = ['id', 'username', 'role', 'phone_number', 'email', 'first_name', 'last_name', 'subject', 'region', 'district', 'school', 'class_id', 'gender']
     list_filter = ['role', 'gender']
     search_fields = ['username', 'phone_number', 'email']
     
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,15 +3,21 @@ from .models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
+    image = serializers.ImageField(required=False, allow_null=True)
+    
     class Meta:
         model = CustomUser
-        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'subject', 'region', 'school', 'class_id', 'gender']
-
+        fields = ['id', 'username', 'first_name', 'last_name', 'phone_number', 'password', 'email', 'role', 'region', 'school', 'class_id', 'gender', 'subject', 'image', 'district']
+    
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
+        # Parolni alohida olish va olib tashlash
+        password = validated_data.pop('password')
+        
+        # Image maydonini alohida olish va olib tashlash
+        image = validated_data.pop('image', None)
+        
+        user = CustomUser(
+            username=validated_data.get('username'),
             email=validated_data.get('email', ''),
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
@@ -19,12 +25,30 @@ class RegisterSerializer(serializers.ModelSerializer):
             role=validated_data.get('role', 'student'),
             subject=validated_data.get('subject', ''),
             region=validated_data.get('region', ''),
+            district=validated_data.get('district', ''),
             school=validated_data.get('school', ''),
             class_id=validated_data.get('class_id', ''),
             gender=validated_data.get('gender', '')
         )
+        
+        # Parolni o'rnatish
+        user.set_password(password)
+        
+        # Foydalanuvchini saqlash
+        user.save()
+        
+        # Agar rasm bo'lsa, uni saqlash
+        if image:
+            user.image.save(image.name, image, save=True)
+        
         return user
-
+    
+    def get_image(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.image.url) if request else obj.image.url
+        return None
+    
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
