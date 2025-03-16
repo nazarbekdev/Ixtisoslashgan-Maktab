87cb4090261c4fa22fea610aae8864c7277d92f2
nazarbekdev <nqobulov571@gmail.com>
2025-03-16 13:22:08 +0500

    new apps

--- core/urls.py
+++ core/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('news/', include('news.urls')), # news uchun URL’lar
     path('contacts/', include('contacts.urls')), # contacts uchun URL’lar
     path('students/', include('students.urls')), # students uchun URL’lar
+    path('teachers/', include('teachers.urls')), # teachers uchun URL’lar 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
