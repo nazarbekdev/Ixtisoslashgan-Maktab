12abe182cb80eee54c04b12309de1109bcf15929
nazarbekdev <nqobulov571@gmail.com>
2025-03-16 13:26:06 +0500

    added students` Submissions

--- students/models.py
+++ students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import CustomUser
+from teachers.models import Material
 
-# Create your models here.
+class Submission(models.Model):
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='media/submissions/')
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.username} - {self.material.topic}"
+    
+    class Meta:
+        verbose_name = "Student vazifa"
+        verbose_name_plural = "Student vazifalar"
