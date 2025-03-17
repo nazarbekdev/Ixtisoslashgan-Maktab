d0dbdd2a7b1d9122630622cbeab8fa52e3a116d8
nazarbekdev <nqobulov571@gmail.com>
2025-03-17 17:09:54 +0500

    new models

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import StudentProfileAPIView, StudentProfileImageAPIView, SubmitAssignmentAPIView
+from .views import StudentProfileAPIView, StudentProfileImageAPIView, SubmitAssignmentAPIView, TestTypeListCreateAPIView, TestResultListCreateAPIView, TestResultDetailView
 
 urlpatterns = [
     path('profile/', StudentProfileAPIView.as_view(), name='student-profile'),
     path('profile/image/', StudentProfileImageAPIView.as_view(), name='student-profile-image'),
     path('submit-assignment/', SubmitAssignmentAPIView.as_view(), name='submit-assignment'),
+    path('test-types/', TestTypeListCreateAPIView.as_view(), name='test-types'),
+    path('test-results/', TestResultListCreateAPIView.as_view(), name='test-results'),
+    path('test-results/<int:student_id>/', TestResultDetailView.as_view(), name='test-result-detail'),
 ]
