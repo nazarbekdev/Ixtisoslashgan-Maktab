29c389de8aa34b4f78a09864e68b482a49b8efdd
nazarbekdev <nqobulov571@gmail.com>
2025-04-13 21:07:50 +0500

    chat admin

--- chats/admin.py
+++ chats/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import ChatMessage
 
+
 @admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'message', 'reply', 'timestamp']
     list_filter = ['timestamp', 'user']
+    search_fields = ['message']
